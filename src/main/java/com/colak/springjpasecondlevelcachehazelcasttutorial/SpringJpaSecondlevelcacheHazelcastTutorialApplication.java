751f5ebbef03b108aa3c3efcdd17957c8e0e59cc
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-07 11:27:56 +0300

    Initial commit

---
+++ src/main/java/com/colak/springjpasecondlevelcachehazelcasttutorial/SpringJpaSecondlevelcacheHazelcastTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.colak.springjpasecondlevelcachehazelcasttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaSecondlevelcacheHazelcastTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaSecondlevelcacheHazelcastTutorialApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/colak/springjpasecondlevelcachehazelcasttutorial/jpa/Country.java
@@ -0,0 +1,30 @@
+package com.colak.springjpasecondlevelcachehazelcasttutorial.jpa;
+
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import java.util.Objects;
+
+
+@Table(name = "countries")
+@Entity
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE, region = "country")
+
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    private int id;
+
+    private String name;
+
+}
